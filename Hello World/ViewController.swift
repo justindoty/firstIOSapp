285bd2eb502db7a635f4ab40222ab60f43c9d3fa
Justin Doty <justindoty12@gmail.com>
2016-10-03 18:36:52 -0500

    first IOS project

--- Hello World/ViewController.swift
+++ Hello World/ViewController.swift
@@ -15,11 +15,23 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBOutlet weak var background: UIImageView!
+    
+    @IBOutlet weak var name: UIImageView!
+    
+    
+    @IBOutlet weak var welcomebutton: UIButton!
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func welcomePressed(_ sender: AnyObject) {
+        background.isHidden = false
+        name.isHidden = false
+        welcomebutton.isHidden = true
+    }
 
 }
 
